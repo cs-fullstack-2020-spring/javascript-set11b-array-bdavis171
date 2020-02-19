f10430bb80246889f1246b825e2a6a1ca279ea43
bdavis171 <bdavis171@gmail.com>
2020-02-19 11:56:43 -0600

    Work finished

---
+++ script.js
@@ -0,0 +1,29 @@
+//Exercise 1:
+
+//Create an array and a four names to it
+let names = ['Brandon', 'Dewayne', 'Sam', 'Mary' ];
+
+//Print array
+console.log(names);
+
+//Exercise 2:
+
+//Create an array with 5 numbers in it
+let numbers = [1,2,3,4,5];
+
+//Print array
+console.log(numbers);
+
+//Exercise 3:
+
+//Create an array with 4 names
+names2 = ['Seth', 'Kendra', 'Ciara', 'Lauran'];
+
+//Print the third items in the array
+console.log(names2[2]);
+
+//Delete the second item
+names2.splice(1,1);
+
+//Print the third item again
+console.log(names2[2]);
